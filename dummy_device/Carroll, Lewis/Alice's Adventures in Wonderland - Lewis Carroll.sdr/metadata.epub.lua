745f4aaf732ea3ed8617dee4cf783dd33bd7b150
Harm te Molder <mail@harmtemolder.com>
2020-11-12 09:07:37 -0300

    Add KOReader metadata to dummy_device

---
+++ dummy_device/Carroll, Lewis/Alice's Adventures in Wonderland - Lewis Carroll.sdr/metadata.epub.lua
@@ -0,0 +1,123 @@
+-- we can read Lua syntax here!
+return {
+    ["copt_font_gamma"] = 30,
+    ["copt_embedded_css"] = 0,
+    ["copt_font_kerning"] = 3,
+    ["header_font_face"] = "Noto Sans",
+    ["doc_props"] = {
+        ["title"] = "Alice's Adventures in Wonderland",
+        ["keywords"] = "Fantasy",
+        ["language"] = "en",
+        ["authors"] = "Lewis Carroll",
+        ["series"] = "",
+        ["description"] = ""
+    },
+    ["stats"] = {
+        ["title"] = "Alice's Adventures in Wonderland",
+        ["notes"] = 0,
+        ["language"] = "en",
+        ["pages"] = 241,
+        ["md5"] = "dc6cce610b4f29cd199d61c755bfc0ca",
+        ["authors"] = "Lewis Carroll",
+        ["series"] = "",
+        ["highlights"] = 1
+    },
+    ["doc_pages"] = 241,
+    ["bookmarks_sorted"] = true,
+    ["highlights_imported"] = true,
+    ["percent_finished"] = 0.020746887966805,
+    ["copt_h_page_margins"] = {
+        [2] = 50,
+        [1] = 50
+    },
+    ["copt_block_rendering_mode"] = 1,
+    ["font_hinting"] = 2,
+    ["copt_line_spacing"] = 130,
+    ["font_kerning"] = 3,
+    ["copt_render_dpi"] = 96,
+    ["font_size"] = 26,
+    ["highlight"] = {
+        [5] = {
+            [1] = {
+                ["pos0"] = "/body/DocFragment[1]/body/p[7]/text().0",
+                ["datetime"] = "2020-11-11 12:46:55",
+                ["chapter"] = "CHAPTER I. Down the Rabbit-Hole",
+                ["text"] = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do",
+                ["drawer"] = "lighten",
+                ["pos1"] = "/body/DocFragment[1]/body/p[7]/text().103"
+            }
+        }
+    },
+    ["embedded_css"] = false,
+    ["copt_smooth_scaling"] = 1,
+    ["word_spacing"] = {
+        [2] = 75,
+        [1] = 95
+    },
+    ["smooth_scaling"] = true,
+    ["copt_font_weight"] = 0,
+    ["highlight_disabled"] = false,
+    ["nightmode_images"] = true,
+    ["rotation_mode"] = 0,
+    ["copt_sync_t_b_page_margins"] = 0,
+    ["render_mode"] = 0,
+    ["show_overlap_enable"] = false,
+    ["visible_pages"] = 1,
+    ["copt_word_expansion"] = 0,
+    ["copt_visible_pages"] = 1,
+    ["css"] = "./data/epub.css",
+    ["gamma"] = 1,
+    ["copt_nightmode_images"] = 1,
+    ["copt_view_mode"] = 0,
+    ["copt_font_hinting"] = 2,
+    ["copt_t_page_margin"] = 30,
+    ["copt_status_line"] = 1,
+    ["copt_word_spacing"] = {
+        [2] = 75,
+        [1] = 95
+    },
+    ["text_lang_embedded_langs"] = true,
+    ["hyphenation"] = true,
+    ["hyph_trust_soft_hyphens"] = false,
+    ["hyph_soft_hyphens_only"] = false,
+    ["hyph_force_algorithmic"] = false,
+    ["floating_punctuation"] = 0,
+    ["highlight_drawer"] = "lighten",
+    ["copt_rotation_mode"] = 0,
+    ["font_face"] = "FreeSerif",
+    ["inverse_reading_order"] = false,
+    ["text_lang"] = "en-US",
+    ["gamma_index"] = 30,
+    ["partial_md5_checksum"] = "dc6cce610b4f29cd199d61c755bfc0ca",
+    ["copt_b_page_margin"] = 50,
+    ["render_dpi"] = 96,
+    ["font_embolden"] = 0,
+    ["cre_dom_version"] = 20200824,
+    ["page_overlap_style"] = "dim",
+    ["last_xpointer"] = "/body/DocFragment[1]/body/hr[2].0",
+    ["disable_fuzzy_search"] = false,
+    ["copt_embedded_fonts"] = 0,
+    ["word_expansion"] = 0,
+    ["embedded_fonts"] = false,
+    ["bookmarks"] = {
+        [1] = {
+            ["page"] = "/body/DocFragment[1]/body/p[9]/text()[1].0",
+            ["datetime"] = "2020-11-11 12:47:23",
+            ["notes"] = "in CHAPTER I. Down the Rabbit-Hole",
+            ["chapter"] = "CHAPTER I. Down the Rabbit-Hole"
+        },
+        [2] = {
+            ["page"] = "/body/DocFragment[1]/body/p[7]/text().0",
+            ["pos1"] = "/body/DocFragment[1]/body/p[7]/text().103",
+            ["highlighted"] = true,
+            ["chapter"] = "CHAPTER I. Down the Rabbit-Hole",
+            ["notes"] = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do",
+            ["pos0"] = "/body/DocFragment[1]/body/p[7]/text().0",
+            ["datetime"] = "2020-11-11 12:46:55"
+        }
+    },
+    ["line_space_percent"] = 130,
+    ["config_panel_index"] = 1,
+    ["readermenu_tab_index"] = 1,
+    ["copt_font_size"] = 26
+}
