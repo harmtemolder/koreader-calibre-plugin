4d0b58c567186b83f4c7b54801d28618ef02d587
Harm te Molder <mail@harmtemolder.com>
2020-11-14 15:25:54 -0300

    Update dummy_device metadata with notes, rating and review

--- dummy_device/Carroll, Lewis/Alice's Adventures in Wonderland - Lewis Carroll.sdr/metadata.epub.lua
+++ dummy_device/Carroll, Lewis/Alice's Adventures in Wonderland - Lewis Carroll.sdr/metadata.epub.lua
@@ -1,123 +1,149 @@
 -- we can read Lua syntax here!
 return {
-    ["copt_font_gamma"] = 30,
-    ["copt_embedded_css"] = 0,
-    ["copt_font_kerning"] = 3,
-    ["header_font_face"] = "Noto Sans",
-    ["doc_props"] = {
-        ["title"] = "Alice's Adventures in Wonderland",
-        ["keywords"] = "Fantasy",
-        ["language"] = "en",
-        ["authors"] = "Lewis Carroll",
-        ["series"] = "",
-        ["description"] = ""
-    },
-    ["stats"] = {
-        ["title"] = "Alice's Adventures in Wonderland",
-        ["notes"] = 0,
-        ["language"] = "en",
-        ["pages"] = 241,
-        ["md5"] = "dc6cce610b4f29cd199d61c755bfc0ca",
-        ["authors"] = "Lewis Carroll",
-        ["series"] = "",
-        ["highlights"] = 1
-    },
-    ["doc_pages"] = 241,
-    ["bookmarks_sorted"] = true,
-    ["highlights_imported"] = true,
-    ["percent_finished"] = 0.020746887966805,
-    ["copt_h_page_margins"] = {
-        [2] = 50,
-        [1] = 50
-    },
-    ["copt_block_rendering_mode"] = 1,
-    ["font_hinting"] = 2,
-    ["copt_line_spacing"] = 130,
-    ["font_kerning"] = 3,
-    ["copt_render_dpi"] = 96,
-    ["font_size"] = 26,
+    ["smooth_scaling"] = false,
+    ["render_mode"] = 0,
+    ["copt_block_rendering_mode"] = 3,
+    ["nightmode_images"] = true,
+    ["disable_fuzzy_search"] = false,
+    ["copt_font_size"] = 22,
+    ["visible_pages"] = 1,
+    ["copt_font_weight"] = 0,
+    ["copt_font_hinting"] = 2,
+    ["inverse_reading_order"] = false,
+    ["copt_word_expansion"] = 0,
+    ["copt_line_spacing"] = 100,
+    ["copt_font_gamma"] = 15,
+    ["line_space_percent"] = 100,
+    ["cre_dom_version"] = 20200824,
+    ["last_xpointer"] = "/body/DocFragment[4]/body/div[4]/text()[298].42",
+    ["copt_visible_pages"] = 1,
     ["highlight"] = {
-        [5] = {
+        [56] = {
             [1] = {
-                ["pos0"] = "/body/DocFragment[1]/body/p[7]/text().0",
-                ["datetime"] = "2020-11-11 12:46:55",
-                ["chapter"] = "CHAPTER I. Down the Rabbit-Hole",
-                ["text"] = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do",
+                ["datetime"] = "2020-11-14 15:02:42",
+                ["chapter"] = "CHAPTER VII. A Mad Tea-Party",
+                ["pos0"] = "/body/DocFragment[2]/body/p[198]/text().1",
+                ["pos1"] = "/body/DocFragment[2]/body/p[198]/text().117",
                 ["drawer"] = "lighten",
-                ["pos1"] = "/body/DocFragment[1]/body/p[7]/text().103"
+                ["text"] = "And ever since that,' the Hatter went on in a mournful tone, 'he won't do a thing I ask! It's always six o'clock now"
+            }
+        },
+        [54] = {
+            [1] = {
+                ["datetime"] = "2020-11-14 15:02:03",
+                ["chapter"] = "CHAPTER VII. A Mad Tea-Party",
+                ["pos0"] = "/body/DocFragment[2]/body/p[169]/text()[1].0",
+                ["pos1"] = "/body/DocFragment[2]/body/p[169]/text()[1].185",
+                ["drawer"] = "lighten",
+                ["text"] = "The March Hare took the watch and looked at it gloomily: then he dipped it into his cup of tea, and looked at it again: but he could think of nothing better to say than his first remark"
             }
         }
     },
-    ["embedded_css"] = false,
-    ["copt_smooth_scaling"] = 1,
-    ["word_spacing"] = {
-        [2] = 75,
-        [1] = 95
-    },
-    ["smooth_scaling"] = true,
-    ["copt_font_weight"] = 0,
-    ["highlight_disabled"] = false,
-    ["nightmode_images"] = true,
-    ["rotation_mode"] = 0,
-    ["copt_sync_t_b_page_margins"] = 0,
-    ["render_mode"] = 0,
+    ["font_face"] = "Noto Serif",
     ["show_overlap_enable"] = false,
-    ["visible_pages"] = 1,
-    ["copt_word_expansion"] = 0,
-    ["copt_visible_pages"] = 1,
-    ["css"] = "./data/epub.css",
-    ["gamma"] = 1,
-    ["copt_nightmode_images"] = 1,
+    ["word_expansion"] = 0,
     ["copt_view_mode"] = 0,
-    ["copt_font_hinting"] = 2,
-    ["copt_t_page_margin"] = 30,
     ["copt_status_line"] = 1,
+    ["copt_h_page_margins"] = {
+        [2] = 10,
+        [1] = 10
+    },
+    ["copt_t_page_margin"] = 15,
+    ["copt_b_page_margin"] = 15,
     ["copt_word_spacing"] = {
         [2] = 75,
         [1] = 95
     },
+    ["text_lang"] = "en-US",
+    ["highlight_drawer"] = "lighten",
+    ["word_spacing"] = {
+        [2] = 75,
+        [1] = 95
+    },
+    ["font_kerning"] = 3,
+    ["partial_md5_checksum"] = "dc6cce610b4f29cd199d61c755bfc0ca",
+    ["summary"] = {
+        ["modified"] = "2020-11-14",
+        ["note"] = "It’s a classic for a reason",
+        ["rating"] = 5,
+        ["status"] = "complete"
+    },
+    ["readermenu_tab_index"] = 1,
+    ["copt_embedded_fonts"] = 1,
+    ["copt_embedded_css"] = 1,
+    ["copt_render_dpi"] = 96,
+    ["bookmarks"] = {
+        [1] = {
+            ["page"] = "/body/DocFragment[2]/body/p[198]/text().1",
+            ["notes"] = "And ever since that,' the Hatter went on in a mournful tone, 'he won't do a thing I ask! It's always six o'clock now",
+            ["datetime"] = "2020-11-14 15:02:42",
+            ["chapter"] = "CHAPTER VII. A Mad Tea-Party",
+            ["pos0"] = "/body/DocFragment[2]/body/p[198]/text().1",
+            ["pos1"] = "/body/DocFragment[2]/body/p[198]/text().117",
+            ["highlighted"] = true,
+            ["text"] = "Page 56 And ever since that,' the Hatter went on in a mournful tone, 'he won't do a thing I ask! It's always six o'clock now @ 2020-11-14 15:02:42"
+        },
+        [2] = {
+            ["page"] = "/body/DocFragment[2]/body/p[169]/text()[1].0",
+            ["datetime"] = "2020-11-14 15:02:03",
+            ["chapter"] = "CHAPTER VII. A Mad Tea-Party",
+            ["pos0"] = "/body/DocFragment[2]/body/p[169]/text()[1].0",
+            ["pos1"] = "/body/DocFragment[2]/body/p[169]/text()[1].185",
+            ["notes"] = "The March Hare took the watch and looked at it gloomily: then he dipped it into his cup of tea, and looked at it again: but he could think of nothing better to say than his first remark",
+            ["highlighted"] = true
+        },
+        [3] = {
+            ["page"] = "/body/DocFragment[2]/body/h2[3]/text().0",
+            ["notes"] = "in CHAPTER VII. A Mad Tea-Party",
+            ["datetime"] = "2020-11-14 15:01:03",
+            ["chapter"] = "CHAPTER VII. A Mad Tea-Party"
+        }
+    },
+    ["page_overlap_style"] = "dim",
+    ["copt_sync_t_b_page_margins"] = 0,
+    ["copt_smooth_scaling"] = 0,
+    ["copt_rotation_mode"] = 0,
+    ["copt_nightmode_images"] = 1,
+    ["highlight_disabled"] = false,
+    ["highlights_imported"] = true,
+    ["font_embolden"] = 0,
     ["text_lang_embedded_langs"] = true,
     ["hyphenation"] = true,
     ["hyph_trust_soft_hyphens"] = false,
     ["hyph_soft_hyphens_only"] = false,
     ["hyph_force_algorithmic"] = false,
-    ["floating_punctuation"] = 0,
-    ["highlight_drawer"] = "lighten",
-    ["copt_rotation_mode"] = 0,
-    ["font_face"] = "FreeSerif",
-    ["inverse_reading_order"] = false,
-    ["text_lang"] = "en-US",
-    ["gamma_index"] = 30,
-    ["partial_md5_checksum"] = "dc6cce610b4f29cd199d61c755bfc0ca",
-    ["copt_b_page_margin"] = 50,
+    ["gamma_index"] = 15,
+    ["percent_finished"] = 1,
+    ["copt_font_kerning"] = 3,
     ["render_dpi"] = 96,
-    ["font_embolden"] = 0,
-    ["cre_dom_version"] = 20200824,
-    ["page_overlap_style"] = "dim",
-    ["last_xpointer"] = "/body/DocFragment[1]/body/hr[2].0",
-    ["disable_fuzzy_search"] = false,
-    ["copt_embedded_fonts"] = 0,
-    ["word_expansion"] = 0,
-    ["embedded_fonts"] = false,
-    ["bookmarks"] = {
-        [1] = {
-            ["page"] = "/body/DocFragment[1]/body/p[9]/text()[1].0",
-            ["datetime"] = "2020-11-11 12:47:23",
-            ["notes"] = "in CHAPTER I. Down the Rabbit-Hole",
-            ["chapter"] = "CHAPTER I. Down the Rabbit-Hole"
-        },
-        [2] = {
-            ["page"] = "/body/DocFragment[1]/body/p[7]/text().0",
-            ["pos1"] = "/body/DocFragment[1]/body/p[7]/text().103",
-            ["highlighted"] = true,
-            ["chapter"] = "CHAPTER I. Down the Rabbit-Hole",
-            ["notes"] = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do",
-            ["pos0"] = "/body/DocFragment[1]/body/p[7]/text().0",
-            ["datetime"] = "2020-11-11 12:46:55"
-        }
-    },
-    ["line_space_percent"] = 130,
+    ["rotation_mode"] = 0,
+    ["floating_punctuation"] = 0,
+    ["bookmarks_sorted"] = true,
     ["config_panel_index"] = 1,
-    ["readermenu_tab_index"] = 1,
-    ["copt_font_size"] = 26
+    ["header_font_face"] = "Noto Sans",
+    ["doc_props"] = {
+        ["authors"] = "Lewis Carroll",
+        ["keywords"] = "Fantasy",
+        ["title"] = "Alice's Adventures in Wonderland",
+        ["language"] = "en",
+        ["series"] = "",
+        ["description"] = ""
+    },
+    ["stats"] = {
+        ["title"] = "Alice's Adventures in Wonderland",
+        ["notes"] = 1,
+        ["md5"] = "dc6cce610b4f29cd199d61c755bfc0ca",
+        ["language"] = "en",
+        ["pages"] = 113,
+        ["highlights"] = 2,
+        ["series"] = "",
+        ["authors"] = "Lewis Carroll"
+    },
+    ["doc_pages"] = 113,
+    ["gamma"] = 1,
+    ["embedded_css"] = true,
+    ["css"] = "./data/epub.css",
+    ["font_hinting"] = 2,
+    ["font_size"] = 22,
+    ["embedded_fonts"] = true
 }
