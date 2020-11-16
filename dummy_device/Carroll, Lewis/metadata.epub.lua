b2b4c6523176828ef858162d9715e520a526af0c
Harm te Molder <mail@harmtemolder.com>
2020-11-16 18:56:05 -0300

    Set up a more elaborate dummy device and library

---
+++ dummy_device/Carroll, Lewis/metadata.epub.lua
@@ -0,0 +1,146 @@
+-- we can read Lua syntax here!
+return {
+    ["highlights_imported"] = true,
+    ["render_mode"] = 0,
+    ["embedded_css"] = true,
+    ["disable_fuzzy_search"] = false,
+    ["embedded_fonts"] = true,
+    ["copt_block_rendering_mode"] = 3,
+    ["smooth_scaling"] = false,
+    ["page_overlap_style"] = "dim",
+    ["nightmode_images"] = true,
+    ["cre_dom_version"] = 20200824,
+    ["last_xpointer"] = "/body/DocFragment[9]/body/div/h2/text()[1].0",
+    ["highlight_disabled"] = false,
+    ["bookmarks"] = {
+        [1] = {
+            ["datetime"] = "2020-11-16 18:52:11",
+            ["page"] = "/body/DocFragment[8]/body/div/h2/text()[1].0",
+            ["chapter"] = "CHAPTER VI. Pig and Pepper",
+            ["notes"] = "in CHAPTER VI. Pig and Pepper"
+        },
+        [2] = {
+            ["text"] = "Higlight with note: Page 48 “You!” said the Caterpillar contemptuously. “Who are you?” @ 2020-11-16 18:51:04",
+            ["page"] = "/body/DocFragment[7]/body/div/p[12]/text()[1].0",
+            ["chapter"] = "CHAPTER V. Advice from a Caterpillar",
+            ["highlighted"] = true,
+            ["datetime"] = "2020-11-16 18:51:04",
+            ["pos0"] = "/body/DocFragment[7]/body/div/p[12]/text()[1].0",
+            ["pos1"] = "/body/DocFragment[7]/body/div/p[12]/text()[2].1",
+            ["notes"] = "“You!” said the Caterpillar contemptuously. “Who are you?”"
+        },
+        [3] = {
+            ["page"] = "/body/DocFragment[7]/body/div/h2/text()[1].0",
+            ["chapter"] = "CHAPTER V. Advice from a Caterpillar",
+            ["highlighted"] = true,
+            ["datetime"] = "2020-11-16 18:50:53",
+            ["pos0"] = "/body/DocFragment[7]/body/div/h2/text()[1].0",
+            ["pos1"] = "/body/DocFragment[7]/body/div/h2/text()[2].26",
+            ["notes"] = "CHAPTER V. Advice from a Caterpillar"
+        }
+    },
+    ["inverse_reading_order"] = false,
+    ["line_space_percent"] = 100,
+    ["css"] = "./data/epub.css",
+    ["percent_finished"] = 0.45512820512821,
+    ["rotation_mode"] = 0,
+    ["partial_md5_checksum"] = "9c928bc227e2011e85f931ca159ff710",
+    ["font_size"] = 22,
+    ["copt_embedded_fonts"] = 1,
+    ["copt_embedded_css"] = 1,
+    ["copt_rotation_mode"] = 0,
+    ["copt_render_dpi"] = 96,
+    ["copt_sync_t_b_page_margins"] = 0,
+    ["copt_h_page_margins"] = {
+        [1] = 10,
+        [2] = 10
+    },
+    ["copt_t_page_margin"] = 15,
+    ["copt_smooth_scaling"] = 0,
+    ["copt_word_spacing"] = {
+        [1] = 95,
+        [2] = 75
+    },
+    ["copt_nightmode_images"] = 1,
+    ["word_spacing"] = {
+        [1] = 95,
+        [2] = 75
+    },
+    ["header_font_face"] = "Noto Sans",
+    ["copt_font_size"] = 22,
+    ["font_embolden"] = 0,
+    ["text_lang"] = "en-US",
+    ["copt_font_hinting"] = 2,
+    ["copt_font_kerning"] = 3,
+    ["copt_word_expansion"] = 0,
+    ["copt_line_spacing"] = 100,
+    ["copt_font_gamma"] = 15,
+    ["gamma"] = 1,
+    ["render_dpi"] = 96,
+    ["font_face"] = "Noto Serif",
+    ["visible_pages"] = 1,
+    ["show_overlap_enable"] = false,
+    ["copt_view_mode"] = 0,
+    ["highlight"] = {
+        [47] = {
+            [1] = {
+                ["drawer"] = "lighten",
+                ["chapter"] = "CHAPTER V. Advice from a Caterpillar",
+                ["datetime"] = "2020-11-16 18:50:53",
+                ["pos0"] = "/body/DocFragment[7]/body/div/h2/text()[1].0",
+                ["text"] = "CHAPTER V. Advice from a Caterpillar",
+                ["pos1"] = "/body/DocFragment[7]/body/div/h2/text()[2].26"
+            }
+        },
+        [48] = {
+            [1] = {
+                ["drawer"] = "lighten",
+                ["chapter"] = "CHAPTER V. Advice from a Caterpillar",
+                ["datetime"] = "2020-11-16 18:51:04",
+                ["pos0"] = "/body/DocFragment[7]/body/div/p[12]/text()[1].0",
+                ["text"] = "“You!” said the Caterpillar contemptuously. “Who are you?”",
+                ["pos1"] = "/body/DocFragment[7]/body/div/p[12]/text()[2].1"
+            }
+        }
+    },
+    ["copt_status_line"] = 1,
+    ["copt_b_page_margin"] = 15,
+    ["readermenu_tab_index"] = 4,
+    ["copt_font_weight"] = 0,
+    ["bookmarks_sorted"] = true,
+    ["stats"] = {
+        ["authors"] = "Lewis Carroll",
+        ["series"] = "",
+        ["title"] = "Alice's Adventures in Wonderland",
+        ["highlights"] = 2,
+        ["notes"] = 0,
+        ["md5"] = "9c928bc227e2011e85f931ca159ff710",
+        ["language"] = "en",
+        ["pages"] = 156
+    },
+    ["hyph_soft_hyphens_only"] = false,
+    ["font_hinting"] = 2,
+    ["doc_pages"] = 156,
+    ["copt_visible_pages"] = 1,
+    ["text_lang_embedded_langs"] = true,
+    ["config_panel_index"] = 1,
+    ["gamma_index"] = 15,
+    ["font_kerning"] = 3,
+    ["hyphenation"] = true,
+    ["hyph_trust_soft_hyphens"] = false,
+    ["doc_props"] = {
+        ["authors"] = "Lewis Carroll",
+        ["series"] = "",
+        ["title"] = "Alice's Adventures in Wonderland",
+        ["description"] = "",
+        ["language"] = "en",
+        ["keywords"] = "Fantasy fiction\
+Children's stories\
+Imaginary places -- Juvenile fiction\
+Alice (Fictitious character from Carroll) -- Juvenile fiction"
+    },
+    ["hyph_force_algorithmic"] = false,
+    ["floating_punctuation"] = 0,
+    ["word_expansion"] = 0,
+    ["highlight_drawer"] = "lighten"
+}
